7e69d12ef348837d985c60f6a816db1c90b3e5a4
dm <dm@quartsoft.com>
2014-04-01 19:56:59 +0300

    added monitrc definition

---
+++ definitions/monitrc.rb
@@ -0,0 +1,25 @@
+# reload: Reload monit so it notices the new service.  :delayed (default) or :immediately.
+# action: :enable To create the monitoring config (default), or :disable to remove it.
+# variables: Hash of instance variables to pass to the ERB template
+# template_cookbook: the cookbook in which the configuration resides
+# template_source: filename of the ERB configuration template, defaults to <LWRP Name>.conf.erb
+define :monitrc, :action => :enable, :reload => :delayed, :variables => {}, :template_cookbook => "monit", :template_source => nil do
+  params[:template_source] ||= "#{params[:name]}.monitrc.erb"
+  if params[:action] == :enable
+    template "#{node["monit"]["includes_dir"]}/#{params[:name]}.conf" do
+      owner "root"
+      group "root"
+      mode 0644
+      source params[:template_source]
+      cookbook params[:template_cookbook]
+      variables params[:variables]
+      notifies :restart, resources(:service => "monit"), params[:reload]
+      action :create
+    end
+  else
+    template "#{node["monit"]["includes_dir"]}/#{params[:name]}.conf" do
+      action :delete
+      notifies :restart, resources(:service => "monit"), params[:reload]
+    end
+  end
+end
